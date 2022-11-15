499a11d923939b380ff96133027a849abcff4e09
Craig MacIntyre <craig@boundary.co.uk>
2022-11-15 23:40:02 +0000

    Added Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import './app.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { AdminScreen } from './components/AdminScreen/AdminScreen'
 import { StrictMode } from 'react'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-7W2Y583PM9')
+ReactGA.send('pageview')
 
 function App() {
   return (
